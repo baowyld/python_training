b408ae5c67b7dfaab10443fdac21ad600af5a0d2
Dmitriy Gaevskiy <bao.wyld@gmail.com>
2018-02-22 11:35:36 +0200

    Added Point class Added geometry.py for list operations trainings

--- fixture/group.py
+++ fixture/group.py
@@ -39,11 +39,13 @@ class GroupHelper:
 
     def return_to_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def modify_first_group(self, new_group_data):
         wd = self.app.wd
@@ -56,3 +58,8 @@ class GroupHelper:
         # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+
+    def count(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        return len(wd.find_elements_by_name("selected[]"))
